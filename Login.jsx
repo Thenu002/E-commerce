1774c128d4d8bcd06d1282006034ed12b198c789
Thenu002 <125337412+Thenu002@users.noreply.github.com>
2023-12-26 16:01:23 +0530

    Add files via upload

--- Login.jsx
+++ Login.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import './Login.css';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Footer, Navbar } from "../components";
 
-function Login() {
+const Login = () => {
   return (
-    <div className='wrapper'>
-      <form action="">
-        <h1>LOGIN</h1>
-        <div className="input-box">
-            <input type="text" placeholder="Username" required />
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">Login</h1>
+        <hr />
+        <div class="row my-4 h-100">
+          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
+            <form>
+              <div class="my-3">
+                <label for="display-4">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                />
+              </div>
+              <div class="my-3">
+                <label for="floatingPassword display-4">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                />
+              </div>
+              <div className="my-3">
+                <p>New Here? <Link to="/register" className="text-decoration-underline text-info">Register</Link> </p>
+              </div>
+              <div className="text-center">
+                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="input-box">
-            <input type="password" placeholder="Password" required />
-        </div>
-       
-        <button type="submit">Login</button>
-      </form>
-      <p>
-        <h3>Don't have an account?{' '}
-        <Link to="/Signin">Sign up</Link></h3>
-      </p>
-    </div>
+      </div>
+      <Footer />
+    </>
   );
-}
+};
 
 export default Login;
